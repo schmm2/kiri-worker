a7bf822d5bcf4c00eb93fb7fec705376e31b3c78
Martin Schmidli <m.schmidli@smartit.ch>
2022-09-18 22:22:55 +0200

    add staging mode

--- Modules/GetDbContext/GetDbContext.psm1
+++ Modules/GetDbContext/GetDbContext.psm1
@@ -4,7 +4,21 @@ function Get-DbContext() {
     $accountName = $env:CosmosDBAccountName
     $dbName = $env:CosmosDBName
 
-    $cosmosDbContext = New-CosmosDbContext -Account $accountName -Database $dbName -Key $primaryKey
+    # Create connection to sql instance, not the specific database
+    $cosmosDbContext = New-CosmosDbContext -Account $accountName -Key $primaryKey
 
+    # Get all DBs
+    $activeDatabases = Get-CosmosDbDatabase -Context $cosmosDbContext
+
+    if($activeDatabases.id -eq $dbName){
+        Write-Host "Datbase $dbName already exists"
+    }else{
+        Write-Host "Database $dbName does not exist yet"
+        New-CosmosDbDatabase -Context $cosmosDbContext -Id $dbName
+    }
+
+    # Create connection to actual database
+    $cosmosDbContext = New-CosmosDbContext -Account $accountName -Key $primaryKey -Database $dbName
+    
     return $cosmosDbContext
 }
--- TRG0001StageDatabase/run.ps1
+++ TRG0001StageDatabase/run.ps1
@@ -3,27 +3,107 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
+Write-Host "TRG0001StageDatabase: start"
+
+##################################
+# Create Database Collections
+##################################
 
 $cosmosDbContext = Get-DbContext
-$query = "SELECT * FROM tenant c"
-$items = Get-CosmosDbDocument -Context $cosmosDbContext -CollectionId 'tenant' -Query $query -QueryEnableCrossPartition $true
 
-Write-Host $items
+$collectionsToCreate = @('configuration', 'configurationType', 'configurationVersion', 'job', 'msGraphResource', 'device', 'deviceVersion', 'deviceWarranty', 'tenant', 'deployment')
+$existingCollections = Get-CosmosDbCollection -Context $cosmosDbContext
 
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
+foreach ($collectionToCreate in $collectionsToCreate) {
+    if ($existingCollections.id -eq $collectionToCreate) {
+        Write-Host "$collectionToCreate already exists"
+    }
+    else {
+        Write-Host "$collectionToCreate not found, create now"
+        New-CosmosDbCollection -Context $cosmosDbContext -Id $collectionToCreate -PartitionKey 'id' 
+    }
 }
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-$body = $items | ConvertTo-Json
+##################################
+# Fill Default Values
+##################################
 
+$msGraphResources = Get-Content "static\msgraphresources.json" | ConvertFrom-Json
+Write-Host "loaded $($msGraphResources.length) graph resources from file"
+
+$newMsGraphResources = 0
+$newConfigurationTypes = 0
+
+foreach($msGraphResource in $msGraphResources){
+    $msGraphResourceDbId = $null
+    $query = "SELECT * FROM msGraphResource c WHERE (c.name = '$($msGraphResource.name)')"
+    $docs = Get-CosmosDbDocument -Context $cosmosDbContext -CollectionId 'msGraphResource' -Query $query -QueryEnableCrossPartition $true -MaxItemCount 1
+    
+    # resource does already exist
+    if($docs.length -gt 0){
+        Write-Host "msGraphResource $($msGraphResource.name) already exists"
+        $msGraphResourceDbId = $docs[0].id
+    }else{
+        Write-Host "msGraphResource $($msGraphResource.name) not found"
+
+        $id = $([Guid]::NewGuid().ToString())
+
+        # Todo: Find a better way to escape the data
+        $msGraphResourceCreated = @"
+        {
+            `"id"`: `"$id`",
+            `"name`": `"$($msgraphResource.name)`",
+            `"resource`": `"$($msgraphResource.resource)`",
+            `"version`": `"$($msgraphResource.version)`",
+            `"category`": `"$($msgraphResource.category ? $msgraphResource.category : "configuration")`",
+            `"expandAttributes`": `"$($msgraphResource.expandAttributes ? $msgraphResource.expandAttributes: "[]")`",
+            `"transformRulesCreate`": `"$($msgraphResource.transformRulesCreate ? $msgraphResource.transformRulesCreate: "[]")`",
+            `"transformRulesPatch`": `"$($msgraphResource.transformRulesPatch ? $msgraphResource.transformRulesPatch: "[]")`",
+            `"nameAttribute`": `"$($msgraphResource.nameAttribute ? $msgraphResource.nameAttribute : "displayName")`"
+          }
+"@
+
+        $newMsGraphResource = New-CosmosDbDocument -Context $cosmosDbContext -CollectionId 'msGraphResource' -DocumentBody $msGraphResourceCreated -PartitionKey $id   
+        $msGraphResourceDbId = $newMsGraphResource.id
+        $newMsGraphResources++
+    }
+
+    foreach($configurationType in $msGraphResource.configurationTypes){
+        $query = "SELECT * FROM configurationType c WHERE (c.name = '$($configurationType.name)')"
+        $docs = Get-CosmosDbDocument -Context $cosmosDbContext -CollectionId 'configurationType' -Query $query -QueryEnableCrossPartition $true -MaxItemCount 1
+        
+        if($docs.length -gt 0){
+            Write-Host "configurationtype $($configurationType.name) already exists"
+        }else{
+            Write-Host "configurationtype$($configurationType.name) not found"
+
+            $id = $([Guid]::NewGuid().ToString())
+
+            $configurationTypeCreated = @"
+            {
+                `"id`": `"$id`",
+                `"name`": `"$($configurationType.name)`",
+                `"platform`": `"$($configurationType.platform)`",
+                `"category`": `"$($configurationType.category)`",
+                `"msGraphResource`": `"$msGraphResourceDbId`"
+            }
+"@
+            New-CosmosDbDocument -Context $cosmosDbContext -CollectionId 'configurationType' -DocumentBody $configurationTypeCreated -PartitionKey $id   
+            $newConfigurationTypes++
+        }
+    }
+}
+
+
+$body = @"
+{
+    "newConfigurationTypes": $newConfigurationTypes,
+    "newMsGraphResources": $newMsGraphResources
+}
+"@
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body = $body
+    Body       = $body
 })
